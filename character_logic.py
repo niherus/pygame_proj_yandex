668a37d3d1277bae9ab5e60b3b8a118b52ffa296
Tyulenev Nikita <tnb6@yandex.ru>
2024-01-11 12:02:38 +0300

    add borders

--- character_logic.py
+++ character_logic.py
@@ -11,53 +11,64 @@ class Player:
         self.screen = screen
         self.move_frame = 200, 200
         self.speed_mv = 5
-        self.speed_rt = 2
+        self.speed_rt = 1
     def import_images(self, path, scale):
         images = []
         for i in range(42):
             img = pygame.image.load(f'{path}\\{i}.png')
             images.append(pygame.transform.scale_by(img, scale))
         return images
+
     def draw(self):
         for i, img in enumerate(self.frames):
             r_img = pygame.transform.rotate(img, self.angle)
             rect = r_img.get_rect()
             rect.center = (self.pos[0], self.pos[1] - i * self.scale)
             self.screen.blit(r_img, rect)
-        print(self.pos, self.map.st_pos, self.map.borders)
+        #print(self.map.get_cell_pos(self.pos))
     def rotate_space(self):
         if self.rotate == 'left':
             self.angle -= self.speed_rt
 
         if self.rotate == 'right':
             self.angle += self.speed_rt
+    def inside_map(self, x, y):
+
+        x_calc1, y_calc1 = (self.map.borders[1][0] - x + self.map.st_pos[0], self.map.borders[1][1] - y + self.map.st_pos[1])
+        x_calc2, y_calc2 = (self.map.borders[2][0] - x + self.map.st_pos[0], self.map.borders[2][1] - y + self.map.st_pos[1])
+        a1, a2 = math.acos(-1 * y_calc1 / math.hypot(x_calc1, y_calc1)) * 180 / math.pi, math.acos(y_calc2 / math.hypot(x_calc2, y_calc2)) * 180 / math.pi
+        return True
+
+
     def move_space(self):
         dx, dy = self.speed_mv * math.cos(self.angle * math.pi / 180), self.speed_mv * math.sin(self.angle * math.pi / 180)
+        x, y = self.pos
         if self.move == 'forward':
-            x, y = self.pos
-            if self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0] and self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
-                self.pos = x - dx, y + dy
-            elif not self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0] and not self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
-                self.map.move(dx, -dy)
-            elif not self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0]:
-                self.pos = x, y + dy
-                self.map.move(dx, 0)
-            elif not self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
-                self.pos = x - dx, y
-                self.map.move(0, -dy)
+
+            if self.map.inside_map((x - dx, y + dy)):
+                if self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0] and self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
+                    self.pos = x - dx, y + dy
+                elif not self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0] and not self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
+                    self.map.move(dx, -dy)
+                elif not self.move_frame[0] < x - dx < self.screen.get_width() - self.move_frame[0]:
+                    self.pos = x, y + dy
+                    self.map.move(dx, 0)
+                elif not self.move_frame[1] < y + dy < self.screen.get_height() - self.move_frame[1]:
+                    self.pos = x - dx, y
+                    self.map.move(0, -dy)
 
         if self.move == 'backward':
-            x, y = self.pos
-            if self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0] and self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
-                self.pos = x + dx, y - dy
-            elif not self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0] and not self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
-                self.map.move(-dx, dy)
-            elif not self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0]:
-                self.pos = x, y - dy
-                self.map.move(-dx, 0)
-            elif not self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
-                self.pos = x + dx, y
-                self.map.move(0, dy)
+            if self.map.inside_map((x + dx, y - dy)):
+                if self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0] and self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
+                    self.pos = x + dx, y - dy
+                elif not self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0] and not self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
+                    self.map.move(-dx, dy)
+                elif not self.move_frame[0] < x + dx < self.screen.get_width() - self.move_frame[0]:
+                    self.pos = x, y - dy
+                    self.map.move(-dx, 0)
+                elif not self.move_frame[1] < y - dy < self.screen.get_height() - self.move_frame[1]:
+                    self.pos = x + dx, y
+                    self.map.move(0, dy)
             #self.pos = x + dx, y - dy
 
 if __name__ == '__main__':
--- map_generating_logic.py
+++ map_generating_logic.py
@@ -6,7 +6,7 @@ class Map_generator:
 
         self.screen = screen
         self.size = width, height
-        self.plat = pygame.transform.scale_by(pygame.image.load('tile_test.png'), 0.5)
+        self.plat = pygame.transform.scale_by(pygame.image.load('ntile.png'), 0.5)
         self.rect = self.plat.get_rect()
         self.w, self.h = self.rect.size
         self.st_pos = -width * self.w / 2 + self.screen.get_width() / 2, -height * self.h / 4 + self.screen.get_height() / 2
@@ -20,26 +20,51 @@ class Map_generator:
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 self.rect.center = (mw // 2 + (i * self.w + j * self.w * -1) / 2,
-                                    self.h // 2 + (i * 0.5 * self.h + j * 0.5 * self.h) / 2)
+                                    self.h // 2 + (i * 0.5773502691896257 * self.h + j * 0.5773502691896257 * self.h) / 2)
                 self.main_surf.blit(self.plat, self.rect)
-
         self.borders = (
             (round(mw // 2 + (0 * self.w + 0 * self.w * -1) / 2),
-             round(self.h // 2  + (0 * 0.5 * self.h + 0 * 0.5 * self.h) / 2)),
-            (round(mw // 2 + (0 * self.w + (self.size[1] - 1) * self.w * -1) / 2),
-             round(self.h // 2  + (0 * 0.5 * self.h + (self.size[1] - 1) * 0.5 * self.h) / 2)),
-            (round(mw // 2 + ((self.size[0] - 1) * self.w + 0 * self.w * -1) / 2),
-             round(self.h // 2  + ((self.size[0] - 1) * 0.5 * self.h + 0 * 0.5 * self.h) / 2)),
+             round(self.h // 2  + (0 * 0.5773502691896257 * self.h + 0 * 0.5773502691896257 * self.h) / 2) - 50),
+            (round(mw // 2 + (0 * self.w + (self.size[1]) * self.w * -1) / 2),
+             round(self.h // 2  + (0 * 0.5773502691896257 * self.h + (self.size[1] ) * 0.5773502691896257 * self.h) / 2) - 50),
+            (round(mw // 2 + ((self.size[0]) * self.w + 0 * self.w * -1) / 2),
+             round(self.h // 2  + ((self.size[0]) * 0.5773502691896257 * self.h + 0 * 0.5773502691896257 * self.h) / 2 ) - 50),
             (round(mw // 2 + ((self.size[0] - 1) * self.w + (self.size[1] - 1) * self.w * -1) / 2),
-             round(self.h // 2  + ((self.size[0] - 1) * 0.5 * self.h + (self.size[1] - 1) * 0.5 * self.h) / 2))
+             round(self.h // 2  + ((self.size[0]) * 0.5773502691896257 * self.h + (self.size[1]) * 0.5773502691896257 * self.h) / 2) - 50)
         )
+        print(*self.borders, sep='\n')
+        pygame.draw.circle(self.main_surf, (255, 0, 0), self.borders[0], 20)
+        pygame.draw.circle(self.main_surf, (0, 255, 0), self.borders[1], 20)
+        pygame.draw.circle(self.main_surf, (0, 0, 255), self.borders[2], 20)
+        pygame.draw.circle(self.main_surf, (255, 255, 0), self.borders[3], 20)
     def move(self, dx, dy):
         self.st_pos = self.st_pos[0] + dx, self.st_pos[1] + dy
 
     def draw(self):
         self.screen.blit(self.main_surf, self.st_pos)
 
+    def distance(self, p1, p2, p0):
+        dx, dy = p2[0] - p1[0], p2[1] - p1[1]
+        return abs(dy * p0[0] - dx * p0[1] + p2[0] * p1[1] - p2[1] * p1[0]) / math.hypot(dx, dy)
 
+    def get_cell_pos(self, pos):
+        x, y = pos[0] - self.st_pos[0], pos[1] - self.st_pos[1]
+        dx = (self.distance(self.borders[0], self.borders[1], (x, y)) / 0.577) / 173.2
+        dy = (self.distance(self.borders[0], self.borders[2], (x, y)) / 0.577) / 173.2
+        return int(dx), int(dy)
+
+    def inside_map(self, pos):
+        x, y = pos[0] - self.st_pos[0], pos[1] - self.st_pos[1]
+        x1, x2, x3 = self.borders[1][0],  self.borders[0][0], self.borders[2][0]
+        y1, y2, y3 = self.borders[1][1], self.borders[0][1], self.borders[2][1]
+        shift = 50
+
+        if x1 < x < x2 and abs(y - y1) < x / (3 ** 0.5) - shift:
+            return True
+        elif x2 <= x < x3 and abs(y - y1) < (x3 - x) / (3 ** 0.5) - shift:
+            return True
+
+        return False
 
 
 if __name__ == '__main__':
